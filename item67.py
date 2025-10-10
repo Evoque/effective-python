2c6950b21f41505c130a22d1aca6326c17b39402
Evoque <rubievoque@gmail.com>
2025-10-10 14:34:42 +0800

    feat: finish item67

--- item67.py
+++ item67.py
@@ -1,6 +1,7 @@
 
 
-# import subprocess
+
+import subprocess
 
 # result = subprocess.run(
 #     ["echo", "Hello from the child!"],
@@ -8,100 +9,86 @@
 #     encoding="utf-8",
 # )
 
-# result.check_returncode() # No exception means it exited cleanly
+# # No exception means it exited cleanly
+# result.check_returncode()
 
-# print(result.stdout)
+# print("*" * 20)
+# print(f'result.stdout: {result.stdout}')
+# print(f'result: {type(result)}')
+# print(f'stdout: {type(result.stdout)}')
+# print("*" * 20)
 
 
-''' Decoupling the child process from the parent frees up the parent process to run many child processes in parallel. '''
-# import time
-# import subprocess 
 
-# start = time.perf_counter()
-# sleep_procs = []
-# for _ in range(10):
-#     proc = subprocess.Popen(["sleep", "1"])
-#     sleep_procs.append(proc)
+# proc = subprocess.Popen(["sleep", "1"])
+# count = 0
+# while (pp := proc.poll()) is None:
+#     print(f'Working... {count}')
+#     count += 1
     
-# for proc in sleep_procs:
-#     proc.communicate()
+# print(f'Exit, proc.poll(): {pp}') 
+    
 
-# end = time.perf_counter()
-# delta = end - start
-# print(f"Finished in {delta:.10} seconds")
-
-import os
-import subprocess
-
-'''
-通过Popen创建一个子进程, 其中子进程:
-- stdin:  
-- stdout: 
-@TODO: PIPE作为stdin/stdout时， 对PIPE生成的r_fd/w_fd理解的还是不够深。得专门看下PIPE的知识，再看这部分内容。
-'''
+import os 
 
 def run_encrypt(data):
     env = os.environ.copy()
     env["password"] = "zf7ShyBhZOraQDdE/FiZpm/m/8f9X+M1"
     proc = subprocess.Popen(
-        ["openssl", "enc", "-pbkdf2", "-pass", "env:password"],
+        ["openssl", "enc", "-des3", "-pass", "env:password"],
         env=env,
         stdin=subprocess.PIPE,
         stdout=subprocess.PIPE,
     )
-    print(f'fd_id in parent: {proc.stdin.fileno()}')
     proc.stdin.write(data)
-    proc.stdin.flush()  
+    proc.stdin.flush() # Ensure that the child gets input
     return proc
 
 def run_hash(input_stdin):
     return subprocess.Popen(
-        ["openssl", "dgst", "-sha256", "-binary"],
+        ["openssl", "dgst", "-whirlpool", "-binary"],
         stdin=input_stdin,
         stdout=subprocess.PIPE,
     )
-    
+
 encrypt_procs = []
 hash_procs = []
+for _ in range(3):
+    data = os.urandom(100)
 
-for _ in range(1):
-    data = os.urandom(10)
-    
     encrypt_proc = run_encrypt(data)
     encrypt_procs.append(encrypt_proc)
-    
-    # aa = encrypt_proc.stdout.fileno()
-    # print(f'aa: {aa}')
-    # encrypt_proc.stdout.close()
-    # encrypt_proc.stdout = None
-    
-    
-    # hash_proc = run_hash(encrypt_proc.stdout)
-    # hash_procs.append(hash_proc)
-     
 
-    
-import os
-import time 
+    hash_proc = run_hash(encrypt_proc.stdout)
+    hash_procs.append(hash_proc)
 
-# for proc in encrypt_procs:
-#     res,_ = proc.communicate()
-#     print(f'res: {res}')
-#     assert proc.returncode == 0
+    # Ensure that the child consumes the input stream and
+    # the communicate() method doesn't inadvertently steal
+    # input from the child. Also lets SIGPIPE propagate to
+    # the upstream process if the downstream process dies.
+    encrypt_proc.stdout.close()
+    encrypt_proc.stdout = None
     
-# for proc in hash_procs:
-#     out, _ = proc.communicate()
-#     print(out[-10:])
-#     assert proc.returncode == 0
+for proc in encrypt_procs:
+    proc.communicate()
+    assert proc.returncode == 0
 
-while True:
-    print("*" * 20)
-    print(f"PID {os.getpid()} ")
-    for proc in encrypt_procs:
-        print(f"encrypt-PID {proc.pid} ")
+for proc in hash_procs:
+    out, _ = proc.communicate()
+    print(out[-10:])
+    assert proc.returncode == 0
+
+
+proc = subprocess.Popen(["sleep", "10"])
+
+try:
+    proc.communicate(timeout=0.1)
+except subprocess.TimeoutExpired:
+    proc.terminate()
+    proc.wait()
     
-    # for proc in hash_procs:
-    #     print(f"hash-PID {proc.pid} ")
-    
-    print("*" * 20)
-    time.sleep(20)
+print(f'Exit status', proc.poll())
+
+
+# Use the `run` convenience function for simple usage and the `Popen` class for advanced usage like 
+# UNIX-style pipelines.
